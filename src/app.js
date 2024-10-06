8c8ab0b5798df82b6b9bf107a3e31a6ebea2a24b
Alejandro Rojas <cidorcorojas@gmail.com>
2024-10-06 16:51:24 -0300

    initial app and book modeling

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const {config} = require('dotenv')
+config();
+
+const app = express()
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, ()=> {
+    console.log(`Escuchando el puerto ${port}`)
+})
+
